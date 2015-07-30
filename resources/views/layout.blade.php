d27828e9791243ca64e0a238d28cb173a6d0cd0b
DinoLai <dinos80152@gmail.com>
2015-07-30 23:58:03 +0800

    add bootstrap css and js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,16 +5,27 @@
     <meta charset="utf-8">
 
     @section('style')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     @show
 
 </head>
 <body>
+    <header>
+    </header>
+
+    <nav>
+    </nav>
+
+    <section>
     @yield('content')
+    </section>
 
     <footer>
     </footer>
 
     @section('script')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     @show
 
 </body>
